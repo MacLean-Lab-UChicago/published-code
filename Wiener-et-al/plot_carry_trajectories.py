30e625c5d2b6182b0b584a43503f4cfc7de3364a
elizatayw <ewiener@uchicago.edu>
2026-06-30 14:11:15 -0500

    Update plot_carry_trajectories.py

--- Wiener-et-al/plot_carry_trajectories.py
+++ Wiener-et-al/plot_carry_trajectories.py
@@ -9,9 +9,7 @@ from mpl_toolkits.mplot3d import Axes3D
 import utils
 import os
 
-#mouseID = 'mouse46'
 mice = ['mouse549']
-#mice = ['mouse25']
 bodyparts_for_centroid = ['d2knuckle', 'd3knuckle', 'wrist', 'wrist_outer']
 bodyparts = ['d1middle', 'd2tip', 'd2middle', 'd2knuckle', 'd3tip', 'd3middle', 'd3knuckle', 
 'd4tip', 'd4middle', 'wrist', 'wrist_outer', 'elbow', 'elbow_crook', 'pedestal']
@@ -113,19 +111,6 @@ for mouseID in mice:
 	dlc_data=dlc_data[:, :-3, :]
 
 	if plot_trajectories:
-		# fig, ax = plt.subplots(1, 3, subplot_kw={'projection': '3d'})
-		# for carry_type in np.unique(labels_multiday).astype(int):
-		# 	plotting_data=dlc_data[labels_multiday==carry_type, :, :]
-		# 	avg = np.nanmean(plotting_data, axis=0)
-		# 	for bp_idx, bp in enumerate(bodyparts[:-1]):
-		# 		ax[carry_type].plot3D(avg[bp_idx*3, :], -avg[bp_idx*3+1, :], -avg[bp_idx*3+2, :], label=bp)
-		# ax[1].legend(loc='center', bbox_to_anchor=(0, -.25), ncol=len(bodyparts)/2)	
-		# ax[0].set_title('Drop')
-		# ax[1].set_title('Success')
-		# ax[2].set_title('Dry')
-		# fig.suptitle(f'{mouseID} Averaged Carry Trajectories')
-		# plt.show()
-
 		bkg_color = (0, 0, 0, 0.06)
 		fig, ax = plt.subplots(subplot_kw={'projection': '3d'})
 		for carry_type in np.sort(np.unique(labels_multiday).astype(int))[::-1]:
@@ -188,41 +173,6 @@ for mouseID in mice:
 		#ax.set_title(f'{mouseID.capitalize()} Carry Trajectories')
 		fig.savefig(figure_savepath + 'paw centroid trajectory.png', dpi=660)
 		plt.show()
-
-		bkg_color = (0, 0, 0, 0.06)
-		fig, ax = plt.subplots(subplot_kw={'projection': '3d'})
-		alpha=0.03
-		for carry in range(paw_centroid.shape[0]):
-			ax.plot3D(paw_centroid[carry, 0, :], -paw_centroid[carry, 1, :], -paw_centroid[carry, 2, :], color='k', alpha=alpha)
-		mean_carry = np.nanmean(paw_centroid[:, :, :], axis=0)
-		ax.plot3D(mean_carry[0, :], -mean_carry[1, :], -mean_carry[2, :], color='r', lw=2)
-
-
-		# ax.plot3D(ped_x, -ped_y, -ped_z, '*', color='k', label='pedestal')
-		ax.view_init(35, -146)
-
-		# Hide grid lines
-		ax.grid(False)
-
-		# Hide axes ticks
-		ax.set_xticks([])
-		ax.set_yticks([])
-		ax.set_zticks([])
-
-		# Remove axis lines
-		ax.xaxis.line.set_color((1.0, 1.0, 1.0, 0.0))
-		ax.yaxis.line.set_color((1.0, 1.0, 1.0, 0.0))
-		ax.zaxis.line.set_color((1.0, 1.0, 1.0, 0.0))
-
-		# change the background color
-		ax.xaxis.set_pane_color(bkg_color) # White (RGBA: 1 for white, 1 for opaque)
-		ax.yaxis.set_pane_color(bkg_color)
-		ax.zaxis.set_pane_color(bkg_color)
-
-		#ax.set_title(f'{mouseID.capitalize()} Carry Trajectories')
-		fig.savefig(figure_savepath + 'paw centroid trajectory all carries.pdf', dpi=660)
-		plt.show()
-
 
 	if plot_normal:
 		figx, axx = plt.subplots(3, 1)
